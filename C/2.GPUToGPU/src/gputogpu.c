71c9b12e56af89f7829c42d913c17ef809e22fb6
Capelli Ludovic <l.capelli@epcc.ed.ac.uk>
2023-12-11 07:30:56 +0800

    GPU2GPU code updated.

--- C/2.GPUToGPU/src/gputogpu.c
+++ C/2.GPUToGPU/src/gputogpu.c
@@ -17,6 +17,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <mpi.h>
+#include <cuda.h>
+#include <cuda_runtime.h>
+
+void launch_increment(int* array_device, int SIZE);
 
 /**
  * @brief Just prints a desperate hello world message.
@@ -24,8 +28,56 @@
 int main(int argc, char* argv[])
 {
 	// Initialise the MPI program
-	printf("Hello world, I know nothing about other MPI processes yet :'(\n");
+	MPI_Init(&argc, &argv);
+
+	int my_rank;
+	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+
+	const int SIZE = 10;
+	const int SIZE_IN_BYTES = SIZE * sizeof(int);
+	int array_host[SIZE];
+	int* array_device = NULL;
+	cudaMalloc((void**)&array_device, SIZE_IN_BYTES);
+	if(my_rank == 0)
+	{
+		for(int i = 0; i < SIZE; i++)
+		{
+			array_host[i] = i;
+		}
+		
+		// Without GPU-aware MPI
+		// cudaMemcpy(array_device, array_host, SIZE_IN_BYTES, cudaMemcpyHostToDevice);
+		// launch_increment(array_device, SIZE);
+		// cudaMemcpy(array_host, array_device, SIZE_IN_BYTES, cudaMemcpyDeviceToHost);
+		// MPI_Send(array_host, SIZE, MPI_INT, 1, 00, MPI_COMM_WORLD);
+
+		// With GPU-aware MPI
+		cudaMemcpy(array_device, array_host, SIZE_IN_BYTES, cudaMemcpyHostToDevice);
+		launch_increment(array_device, SIZE);
+		MPI_Send(array_device, SIZE, MPI_INT, 1, 0, MPI_COMM_WORLD);
+	}
+	else if(my_rank == 1)
+	{
+		// Without GPU-aware MPI
+		// MPI_Recv(array_host, SIZE, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+		// cudaMemcpy(array_device, array_host, SIZE_IN_BYTES, cudaMemcpyHostToDevice);
+		// launch_increment(array_device, SIZE);
+		// cudaMemcpy(array_host, array_device, SIZE_IN_BYTES, cudaMemcpyDeviceToHost);
+
+		// With GPU-aware MPI
+		MPI_Recv(array_device, SIZE, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+		launch_increment(array_device, SIZE);
+		cudaMemcpy(array_host, array_device, SIZE_IN_BYTES, cudaMemcpyDeviceToHost);
+
+		for(int i = 0; i < SIZE; i++)
+		{
+			printf("%d ", array_host[i]);
+		}
+		printf("\n");
+	}
+
 	// Finalize the MPI program
+	MPI_Finalize();
 
 	return EXIT_SUCCESS;
 }
